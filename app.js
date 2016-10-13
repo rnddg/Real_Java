03f0da7f7b56036951a4f22be163785a3e6309d9
Liam V <ljvasey@gmail.com>
2016-10-13 01:01:04 -0600

    Add comments to app.js, basic 'beans' route

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 var express = require('express');
 var app = express();
 
+app.use(express.static('public'));
+
+// Define Routes
 app.get('/', function(req, res){
   res.send('Real_Java Running');
 });
 
+app.get('/beans/:beanId/:beanName', function(req, res) {
+  res.send(req.params);
+});
+
+// Listen on port 3000
 app.listen(3000, function(){
   console.log('Real_Java running on port 3000');
 })
